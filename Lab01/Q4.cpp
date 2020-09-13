4e01f01f72b747f04da0ea7ea467cb13429809c6
repl.it user <replituser@example.com>
2020-09-13 06:05:29 +0000

    Revise code nomor 5

--- Lab01/Q4.cpp
+++ Lab01/Q4.cpp
@@ -3,22 +3,22 @@ using namespace std;
 
 /* Write a C++ program to compute the total and average of three numbers. 17, 23, 49 */
 
-int main(){
-  //STEP 1: name variable
-  int number1 = 17, number2 = 23, number3 = 49;
-  int total;
-  double average;
+int main()
+{
+  //STEP 1: Name variable
+  float a = 17, b = 23, c = 49;
+  float total;
+  float average;
 
-  //STEP 2: operate
-  //total
-  total = number1 + number2 + number3;
-  
-  //average
+  //STEP 2: Operate total
+  total = a + b + c;
+
+  //STEP 3: Operate average
   average = total / 3;
 
-  //STEP 3: print out result
-  cout<< "Total of 17, 23, 49 = " << total << endl;
-  cout << "Average of 17, 23, 49 = " << average << endl;
+  //STEP 4: Print out result
+  cout << "Total of 17 + 23 + 49 = " << total << endl;
+  cout << "The Average is " << average << endl;
 
   return 0;
 
