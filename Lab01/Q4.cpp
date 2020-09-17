c105fb5dbd6267fdcba8866c977c25f1d1485622
repl.it user <replituser@example.com>
2020-09-17 15:02:46 +0000

    completing the comment

--- Lab01/Q4.cpp
+++ Lab01/Q4.cpp
@@ -6,7 +6,10 @@ using namespace std;
 int main()
 {
   //STEP 1: Name variable
+  //assign 17 to a, 23 to b, and 49 to c
   float a = 17, b = 23, c = 49;
+
+  //declare total and average to be float
   float total;
   float average;
 
@@ -17,7 +20,10 @@ int main()
   average = total / 3;
 
   //STEP 4: Print out result
+  //display total result
   cout << "Total of 17 + 23 + 49 = " << total << endl;
+
+  //display average result
   cout << "The Average is " << average << endl;
 
   return 0;
