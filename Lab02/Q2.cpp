00afe2cd9a5564d1190387003e842782034b8ac6
repl.it user <replituser@example.com>
2020-09-18 03:32:56 +0000

    Answer number 2

--- Lab02/Q2.cpp
+++ Lab02/Q2.cpp
@@ -4,18 +4,21 @@ using namespace std;
 int main()
 {
   //STEP 1: declare variable for age for this year, last year and next year
-  int ageThisYear, ageLastYear, ageNextYear;
+  int stayAgeValue, ageThisYear, ageLastYear, ageNextYear;
 
   //STEP 2: Display request to input age this year
   cout << "Input your age this year = ";
   cin >> ageThisYear;
 
   //STEP 3: Operate value with increment and decrement
+  //input ageThisYear value to stayAgeValue so the value stay
+  stayAgeValue = ageThisYear;
+
   //decrement 1 value to show age last year
   ageLastYear = --ageThisYear;
 
   //increment 1 value to show age next year
-  ageNextYear = ageThisYear;
+  ageNextYear = ++stayAgeValue;
 
   //STEP 4: Display result
   //display ageLastYear
---
+++ Lab02/Q4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  //STEP 1: Declare variable celcius and farenheit to float
+  float celcius, farenheit;
+
+  //STEP 2: Display request to input celcius value
+  cout << "The temperatur in Celcius = ";
+  
+  //STEP 3: Read in value and assign it to celcius
+  cin >> celcius;
+
+  //STEP 4: Operate celcius to farenheit
+  farenheit = ((celcius * (9/5)) + 32);
+
+  //STEP 5: Display result
+  cout << "The temperatur in Farenheit = " << farenheit <<endl;
+
+  return 0;
+}
